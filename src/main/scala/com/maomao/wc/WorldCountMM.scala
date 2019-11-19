8e4347e54a8f26d1f516896bc3a4abc8f4efc018
maohongqi <maohongqi@qianxin.com>
2019-11-19 18:12:44 +0800

    修改路径

--- src/main/scala/com/maomao/wc/WorldCountMM.scala
+++ src/main/scala/com/maomao/wc/WorldCountMM.scala
@@ -6,7 +6,7 @@ object WorldCountMM {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local").setAppName("wordCount")
     val sc = new SparkContext(conf)
-    sc.textFile("D:\\mhq\\sparkdemo\\src\\main\\resources\\wc")
+    sc.textFile("D:\\study\\sparkdemo\\src\\main\\resources\\wc")
       .flatMap(_.split(" "))
       .map((_, 1))
       .reduceByKey(_ + _)
