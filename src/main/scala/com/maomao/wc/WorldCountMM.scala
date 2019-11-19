79ea00681eebab3513bac26435db2aab89dd8cd7
maohongqi <maohongqi@qianxin.com>
2019-11-19 17:51:21 +0800

    新建sparkdemo

---
+++ src/main/scala/com/maomao/wc/WorldCountMM.scala
@@ -0,0 +1,17 @@
+package com.maomao.wc
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WorldCountMM {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("wordCount")
+    val sc = new SparkContext(conf)
+    sc.textFile("D:\\mhq\\sparkdemo\\src\\main\\resources\\wc")
+      .flatMap(_.split(" "))
+      .map((_, 1))
+      .reduceByKey(_ + _)
+      .foreach(println)
+//      .saveAsTextFile("E://tessssssst")
+    sc.stop()
+  }
+}
