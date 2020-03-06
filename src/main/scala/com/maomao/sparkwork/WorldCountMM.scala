3ccd813990f3c9ee1cd5aeae303098a0c440f3c7
maohongqi <maohongqi@qianxin.com>
2020-03-06 11:24:03 +0800

    test

--- src/main/scala/com/maomao/sparkwork/WorldCountMM.scala
+++ src/main/scala/com/maomao/sparkwork/WorldCountMM.scala
@@ -2,16 +2,21 @@ package com.maomao.sparkwork
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+ * @Author maohongqi
+ * @Date 2020/3/6 11:22
+ * @Version 1.0
+ **/
 object WorldCountMM {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local").setAppName("wordCount")
     val sc = new SparkContext(conf)
-    sc.textFile("E://test.txt")
+    sc.textFile("D:\\study\\sparkdemo\\src\\main\\resources\\wc")
       .flatMap(_.split(" "))
       .map((_, 1))
       .reduceByKey(_ + _)
-//      .foreach(println)
-      .saveAsTextFile("E://tessssssst")
+      .foreach(println)
+//      .saveAsTextFile("E://tessssssst")
     sc.stop()
   }
 }
