d71049c9cc31eefbb74ea5cccd3399a8f9b99820
maohongqi <maohongqi@qianxin.com>
2020-04-24 15:58:54 +0800

    sparksql测试类

--- src/main/scala/com/maomao/sparkwork/sparksql.scala
+++ src/main/scala/com/maomao/sparkwork/sparksql.scala
@@ -1,24 +1,17 @@
 package com.maomao.sparkwork
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{SQLContext, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object sparksql {
   case class Person(id: Int, name: String, sex: String, age: Int)
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("sparksql").setMaster("local")
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
-    import sqlContext.implicits._
-    sqlContext.read.textFile()
-    val lineRDD = sc.textFile("E://sparksql.txt").map(_.split(" "))
-    val personRDD = lineRDD.map(x => Person(x(0).toInt, x(1), x(2), x(3).toInt))
-    val personDF = personRDD.toDF()
-    personDF.show
-    personDF.registerTempTable("t_person")
-    sqlContext.sql("select * from t_person order by age desc limit 2").show
-    sqlContext.sql("desc t_person").show
-    sc.stop()
+    val session = SparkSession.builder().config(conf).getOrCreate()
+    val dataFrame = session.read.json("D:\\study\\sparkdemo\\src\\main\\resources\\people.json")
+    dataFrame.createTempView("table")
+    session.sql("select * from table").show()
+    session.stop()
   }
 
 }
--- src/main/scala/com/maomao/sparkwork/sparksqltext.scala
+++ src/main/scala/com/maomao/sparkwork/sparksqltext.scala
@@ -6,12 +6,21 @@ object sparksqltext {
   def main(args: Array[String]): Unit = {
     val sparkSession = SparkSession.builder.master("local").appName("sparksqlexample")
       .getOrCreate()
-    val tt = sparkSession.read.json("E://testdata/data.txt")
-    tt.createOrReplaceTempView {
-      "user"
-    }
-    tt.show()
-    sparkSession.sql("select * from user ").show()
+    // format指定读取csv文件。
+    // header是否指定头部行作为schema。
+    // multiLine在单元格中可能因为字数多有换行，但是不指定这个参数，处理数据时可能会报错。指定这个参数为true，可以将换行的单元格合并为1行。
+    // delimiter 分隔符，默认为逗号,
+    // nullValue 指定一个字符串代表 null 值
+    // quote 引号字符，默认为双引号"
+    // inferSchema 自动推测字段类型
+    val dataFrame = sparkSession.read
+      .option("header", "true")
+      .option("multiLine", true)
+      .option("delimiter",";")
+      .option("inferSchema",true)
+      .csv("D:\\study\\sparkdemo\\src\\main\\resources\\people.csv")
+    dataFrame.createTempView("people")
+    sparkSession.sql("select * from people")
     sparkSession.stop()
 
 
