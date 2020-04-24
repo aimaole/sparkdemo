0a918e8e20c07ce9637d5f88dad27d4d3e38bb9c
maohongqi <maohongqi@qianxin.com>
2020-04-24 18:36:58 +0800

    sparksql测试类

--- src/main/scala/com/maomao/sparkwork/sparksqltext.scala
+++ src/main/scala/com/maomao/sparkwork/sparksqltext.scala
@@ -1,29 +1,23 @@
 package com.maomao.sparkwork
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.{Row, SparkSession}
 
 object sparksqltext {
   def main(args: Array[String]): Unit = {
-    val sparkSession = SparkSession.builder.master("local").appName("sparksqlexample")
-      .getOrCreate()
-    // format指定读取csv文件。
-    // header是否指定头部行作为schema。
-    // multiLine在单元格中可能因为字数多有换行，但是不指定这个参数，处理数据时可能会报错。指定这个参数为true，可以将换行的单元格合并为1行。
-    // delimiter 分隔符，默认为逗号,
-    // nullValue 指定一个字符串代表 null 值
-    // quote 引号字符，默认为双引号"
-    // inferSchema 自动推测字段类型
-    val dataFrame = sparkSession.read
-      .option("header", "true")
-      .option("multiLine", true)
-      .option("delimiter",";")
-      .option("inferSchema",true)
-      .csv("D:\\study\\sparkdemo\\src\\main\\resources\\people.csv")
-    dataFrame.createTempView("people")
-    sparkSession.sql("select * from people")
+    val sparkSession = SparkSession.builder.master("local").appName("sparksqltxt").getOrCreate()
+    val rdd = sparkSession.sparkContext.textFile("D:\\study\\sparkdemo\\src\\main\\resources\\people.txt")
+    val rowrdd = rdd.map(x => x.split(",")).map(attrs => Row(attrs(0), attrs(1).trim.toInt))
+    //创建schema信息
+    val schema = StructType(List(
+      StructField("name", StringType, true),
+      StructField("age", IntegerType, true)
+    ))
+    //创建dataFrame
+    val df = sparkSession.createDataFrame(rowrdd, schema);
+    df.createTempView("person")
+    sparkSession.sql("select * from person").show()
     sparkSession.stop()
-
-
   }
 
 }
