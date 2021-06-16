1f7095d8d111b3dcbeab5fca01f8eb1d5343caf2
337340028 <337340028@qq.com>
2021-06-16 21:45:14 +0800

    test

---
+++ src/main/test/com/maomao/Utils/YamlUtilsTest.java
@@ -0,0 +1,30 @@
+package com.maomao.Utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class YamlUtilsTest {
+
+    @org.junit.jupiter.api.Test
+    void readYaml() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readYamlNotes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveNotseYaml() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveYaml() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void jsonToYamlStr() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void jsonToNoteYamlStr() {
+    }
+}
